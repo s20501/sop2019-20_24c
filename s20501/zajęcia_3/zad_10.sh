a7bb244a3cb0cf06913457f9f9c37b6f7f4f72f4
Marcin Żmuda-Trzebiatowski <s20501@pjwstk.edu.pl>
2020-03-29 22:59:53 +0200

    Kolorowanie skrypt

---
+++ s20501/zajęcia_3/zad_10.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Przygotuj skrypt z funkcją która będzie wypisywała tekst pokolorowany na zadany kolor. Niech funkcja ta przyjmuje dwa argumenty - tekst i kolor.
+
+function color {
+    echo -e "\033[$2m$1\e[0m"
+}
+
+color $1 $2
