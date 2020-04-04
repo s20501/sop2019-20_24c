0a5a5d6c02cd99f4c0ed7f71fce342dece0b3fc0
s20501 <56770915+s20501@users.noreply.github.com>
2020-04-04 12:55:38 +0200

    Update zad_11.sh
    Bug fix

--- s20501/zajęcia_3/zad_11.sh
+++ s20501/zajęcia_3/zad_11.sh
@@ -2,13 +2,13 @@
 
 # Ciąg fibonnaciego rekurencja
 
-function fib() {
+function fib {
     if [ $1 -le 0 ]; then
         echo 0
     elif [ $1 -eq 1 ]; then
         echo 1
     else
-        echo $(($(fib $(($1 - 2))) + $(fib $(($1 - 1)))))
+        echo $[`fib $[$1-2]` + `fib $[$1 - 1]` ]
     fi
 
 }
